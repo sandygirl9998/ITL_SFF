f67a4178454309497bae156262be3c7d662e9224
Akhilesh1198 <akhilesh.cm@lntinfotech.com>
2021-01-11 12:17:54 +0530

    removed updated UserRepository

--- farmerscheme/src/main/java/com/lti/repository/UserRepository.java
+++ farmerscheme/src/main/java/com/lti/repository/UserRepository.java
@@ -1,29 +1,15 @@
 package com.lti.repository;
 
-import org.springframework.stereotype.Repository;
-
 import com.lti.dto.Login;
 
-@Repository
-public class UserRepository extends GenericRepository {
-	public boolean isUserPresent(String email) {
-		boolean b=(Long)
-				entityManager
-				.createQuery("select count(u.userId) from User u where u.emailId = :email")
-				.setParameter("email", email)
-				.getSingleResult() == 1 ? true : false;
-		return b;
-	}
+public interface UserRepository  {
+
 	
-	public int findAccount(Login login) {
-		
-		return  (Integer)entityManager
-				.createQuery("select u.userId from User u WHERE u.emailId=:uname AND u.password=:pwd AND u.role=:rol AND u.status='Approved'")
-				.setParameter("uname", login.getUsername())
-				.setParameter("pwd",login.getPassword())
-				.setParameter("rol", login.getRole()).getSingleResult();
-				
-		
-	}
+	
+	public boolean isUserPresent(String email) ;	
+	public int findAccount(Login login) ;			
+	public <E> E fetch(Class<E> clazz, Object pk) ;
+	
+	
 
 }
---
+++ farmerscheme/src/main/java/com/lti/repository/UserRepositoryImpl.java
@@ -0,0 +1,41 @@
+package com.lti.repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import com.lti.dto.Login;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+
+	@PersistenceContext
+	protected EntityManager entityManager;
+	
+	public boolean isUserPresent(String email) {
+		boolean b=(Long)
+				entityManager
+				.createQuery("select count(u.userId) from User u where u.emailId = :email")
+				.setParameter("email", email)
+				.getSingleResult() == 1 ? true : false;
+		
+		return b;
+	}
+	
+	public int findAccount(Login login) {
+		
+		return  (Integer)entityManager
+				.createQuery("select u.userId from User u WHERE u.emailId=:uname AND u.password=:pwd AND u.role=:rol AND u.status='Approved'")
+				.setParameter("uname", login.getUsername())
+				.setParameter("pwd",login.getPassword())
+				.setParameter("rol", login.getRole()).getSingleResult();				
+		
+	}
+	public <E> E fetch(Class<E> clazz, Object pk) {
+		E e = entityManager.find(clazz, pk); 
+		return e;
+	}
+	
+
+}
