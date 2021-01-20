8550d60ece92b2af0e601d9fcb57d17675acdc60
sandygirl9998 <Sandhiya.K@lntinfotech.com>
2021-01-20 10:53:40 +0530

    Changed mail id

--- farmerscheme/src/main/java/com/lti/controller/UserController.java
+++ farmerscheme/src/main/java/com/lti/controller/UserController.java
@@ -2,6 +2,7 @@ package com.lti.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
